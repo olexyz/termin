ea049d522c6d6f792b73d51e2c4f70337ca96da8
Olesya <32404786+olexyz@users.noreply.github.com>
2019-07-06 17:12:03 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-  $user = "postgres";
-  $pass = "12345678";
-  $db = "termin";
+  $user = "wcezxvczbtdeqc";
+  $pass = "1def5a5d62d33ca99d00329236a0734b7e5cb860abd857c8644c12cda00c8a23";
+  $db = "d8mn1v5711ut84";
   $port = "5432";
-  $host = "localhost";
+  $host = "ec2-54-228-246-214.eu-west-1.compute.amazonaws.com";
   $conn = pg_pconnect ("host=".$host." port=".$port." dbname=".$db." user=".$user." password=".$pass);
   if ( !$conn )  die( "No connection" );
   pg_set_client_encoding($conn, "UNICODE");
